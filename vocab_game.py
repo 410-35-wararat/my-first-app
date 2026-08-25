8b05146bf7d95e0e6b015e2670b29d0b91a72c2e
410-35-wararat <54756@yupparaj.ac.th>
2026-08-25 13:34:19 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -72,11 +72,11 @@ st.divider()
 
 # 3. ช่องรับคำตอบ (ใช้ value ผูกกับตัวแปรตรงๆ เพื่อสั่งเคลียร์ได้)
 ans1 = st.text_input(
-    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
+    "ข้อ 1: An `W a _ e r m _ l on` a day keeps the doctor away. 🍉",
     value=st.session_state.ans1_val,
 )
 ans2 = st.text_input(
-    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
+    "ข้อ 2: Cats love to eat `L a p t o _`. 💻",
     value=st.session_state.ans2_val,
 )
 
